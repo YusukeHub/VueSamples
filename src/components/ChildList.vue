b8f5e0782722309d2773e43ce0a02f290c40df16
YusukeHub <yuusuke.nomura@gmail.com>
2018-12-25 19:01:24 +0900

    lintのindentエラーを解消

--- src/components/ChildList.vue
+++ src/components/ChildList.vue
@@ -1,89 +1,92 @@
 <template>
-  <div>
-    <h2>List Of Tasksk.</h2>
-    <ul>
-      <li v-for="child in children">
-        <component v-bind:is="child.componentName"
-                   v-bind="child"
-                   ref="childParts">
-        </component>
-      </li>
-    </ul>
-  </div>
+    <div>
+        <h2>List Of Tasksk.</h2>
+        <ul>
+            <li v-for="child in children">
+                <component v-bind:is="child.componentName"
+                           v-bind="child"
+                           ref="childParts">
+                </component>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-  import ChildPartsOne from "./ChildPartsOne";
-  import ChildPartsTwo from "./ChildPartsTwo";
+    import ChildPartsOne from "./ChildPartsOne";
+    import ChildPartsTwo from "./ChildPartsTwo";
 
-  export default {
-    name: "ChildList",
-    components: {ChildPartsOne, ChildPartsTwo},
-    data: function () {
-      return {
-        children: [
-          { componentName: 'ChildPartsOne', name: 'Create an child parts IF.', url: ''},
-          { componentName: 'ChildPartsTwo', name: 'run child functions. and show dialog of Result.', url: ''},
-          { componentName: 'ChildPartsOne', name: 'test promise sync tasks.', url: ''}
-        ]
-      }
-    },
-    methods: {
-      // 入力値が不正ならfalseを返す
-      validate: function () {
-        console.log("->ChildList.validate()");
-        let ret = true;
+    export default {
+        name: 'ChildList',
+        components: {ChildPartsOne, ChildPartsTwo},
+        data: function () {
+            return {
+                children: [
+                    {componentName: 'ChildPartsOne', name: 'Create an child parts IF.', url: ''},
+                    {componentName: 'ChildPartsTwo', name: 'run child functions. and show dialog of Result.', url: ''},
+                    {componentName: 'ChildPartsOne', name: 'test promise sync tasks.', url: ''}
+                ]
+            }
+        },
+        methods: {
+            // 入力値が不正ならfalseを返す
+            validate: function () {
+                console.log('->ChildList.validate()');
+                let ret = true;
 
-        // 全てのChildに問い合わせる
-        this.$refs.childParts.forEach(function (item) {
-          if (false === item.validate()) {
-            ret = false;
-          }
-        });
-        return ret;
-      },
-      // 登録前にしなければならない処理を返す(ない場合は長さ0の配列が返される)
-      getRegistPromises: function () {
-        console.log("->ChildList.getRegistPromises()");
-        let promises = [];
+                // 全てのChildに問い合わせる
+                this.$refs.childParts.forEach(function (item) {
+                    if (false === item.validate()) {
+                        ret = false;
+                    }
+                });
+                return ret
+            },
+            // 登録前にしなければならない処理を返す(ない場合は長さ0の配列が返される)
+            getRegistPromises: function () {
+                console.log('->ChildList.getRegistPromises()');
+                let promises = [];
 
-        // 全てのChildに問い合わせる
-        this.$refs.childParts.forEach(function (item) {
-          let ret = item.getRegistPromise();
-          if (ret) {
-            promises.push(ret);
-          }
-        });
-        return promises;
-      },
-      // 登録するConfigを返す
-      buildConfig: function () {
-        console.log("->ChildList.buildConfig()");
-        let configs = [];
+                // 全てのChildに問い合わせる
+                this.$refs.childParts.forEach(function (item) {
+                    let ret = item.getRegistPromise();
+                    if (ret) {
+                        promises.push(ret);
+                    }
+                });
+                return promises;
+            },
+            // 登録するConfigを返す
+            buildConfig: function () {
+                console.log('->ChildList.buildConfig()');
+                let configs = [];
 
-        // 全てのChildに問い合わせる
-        this.$refs.childParts.forEach(function (item) {
-          configs.push(item.buildConfig());
-        });
-        return configs;
-      }
+                // 全てのChildに問い合わせる
+                this.$refs.childParts.forEach(function (item) {
+                    configs.push(item.buildConfig());
+                });
+                return configs;
+            }
+        }
     }
-  }
 </script>
 
 <style scoped>
-  h1, h2 {
-    font-weight: normal;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
--- src/components/ChildPartsTwo.vue
+++ src/components/ChildPartsTwo.vue
@@ -1,47 +1,47 @@
 <template>
-  <a v-bind:href="url" target="_blank"> {{name}} </a>
+    <a v-bind:href="url" target="_blank"> {{name}} </a>
 </template>
 <script>
-  export default {
-    name: 'ChildPartsTwo',
-    props: {
-      name: {default: ''},
-      url: {default: ''}
-    },
-    data: function () {
-      return {calcVal: 2};
-    },
-    methods: {
-      // 入力が正しいか返す
-      validate: function () {
-        console.log("-->  ChildPartsTwo.validate()");
-        return true;
-      },
-      // 表示を再現する設定を返す(注意：この関数はgetRegistPromise()で事前登録しておかないと古い値を返す)
-      buildConfig: function () {
-        console.log("-->  ChildPartsTwo.buildConfig(" + this.calcVal);
-        return this.calcVal;
-      },
-      // 登録前に行う処理をPromiseオブジェクトとして返す
-      getRegistPromise: function () {
-        console.log("-->  ChildPartsTwo.getRegistPromise(" + this.calcVal);
-        return new Promise(this.prepareSave);
-      },
-      // 入力値を返す(注意：この関数はgetRegistPromise()で事前登録しておかないと古い値を返す)
-      getValues: function () {
-        console.log("-->  ChildPartsTwo.getValues(" + this.calcVal);
-        return this.calcVal;
-      },
-      // 保存前の準備をする(添付ファイルのアップロードなど)
-      prepareSave: function (resolve, reject) {
-        let self = this;
-        setTimeout(function () {
-          let rand = Math.floor(Math.random() * 100);
-          console.log("-->  ChildPartsTwo.prepareSave(" + self.calcVal + ' -> ' + rand);
-          self.calcVal = rand;
-          resolve(rand);
-        }, 4000);
-      }
+    export default {
+        name: 'ChildPartsTwo',
+        props: {
+            name: {default: ''},
+            url: {default: ''}
+        },
+        data: function () {
+            return {calcVal: 2};
+        },
+        methods: {
+            // 入力が正しいか返す
+            validate: function () {
+                console.log('-->  ChildPartsTwo.validate()');
+                return true;
+            },
+            // 表示を再現する設定を返す(注意：この関数はgetRegistPromise()で事前登録しておかないと古い値を返す)
+            buildConfig: function () {
+                console.log('-->  ChildPartsTwo.buildConfig(' + this.calcVal);
+                return this.calcVal;
+            },
+            // 登録前に行う処理をPromiseオブジェクトとして返す
+            getRegistPromise: function () {
+                console.log('-->  ChildPartsTwo.getRegistPromise(' + this.calcVal);
+                return new Promise(this.prepareSave);
+            },
+            // 入力値を返す(注意：この関数はgetRegistPromise()で事前登録しておかないと古い値を返す)
+            getValues: function () {
+                console.log('-->  ChildPartsTwo.getValues(' + this.calcVal);
+                return this.calcVal;
+            },
+            // 保存前の準備をする(添付ファイルのアップロードなど)
+            prepareSave: function (resolve, reject) {
+                let self = this;
+                setTimeout(function () {
+                    let rand = Math.floor(Math.random() * 100);
+                    console.log('-->  ChildPartsTwo.prepareSave(' + self.calcVal + ' -> ' + rand);
+                    self.calcVal = rand;
+                    resolve(rand);
+                }, 4000)
+            }
+        }
     }
-  }
 </script>
